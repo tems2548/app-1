04f75b4d6c098c9ca5f80902814bd6cfb69bf522
tems2548 <tems2548@gmail.com>
2023-07-03 15:02:55 +0700

    Initial Commit

---
+++ app-1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  app-1
+//
+//  Created by TemZ on 3/7/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
