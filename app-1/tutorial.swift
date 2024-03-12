06e2e45ef1670691abc3e93765ab5ea2e0d4262e
itzPEKOS <phonlakritc@gmail.com>
2024-03-12 14:08:01 +0700

    tutorial2

--- app-1/tutorial.swift
+++ app-1/tutorial.swift
@@ -16,11 +16,16 @@ struct tutorial: View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(maxWidth: .infinity)
+                    .padding(.bottom, 40)
+                    .padding(.horizontal, 60)
+                Text("RULES")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                 Text("- Transfer all discs from the first tower to the third tower.\nOnly one disc can be moved at a time.\n- Only the top disc of one stack can be transferred to the top of another stack or an empty rod.\n- Larger discs cannot be stacked over smaller ones.")
                     .font(.title2)
             }
             .navigationTitle("Tutorial")
-            
+            .navigationBarTitleDisplayMode(.large)
         }
     }
 }
