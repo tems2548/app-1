14eb07f50aef3a4b6b8a79023e5ce99a7c8b63d7
TemZ <110783208+tems2548@users.noreply.github.com>
2024-03-12 13:23:52 +0700

    fix func stack

--- app-1/tutorial.swift
+++ app-1/tutorial.swift
@@ -10,39 +10,8 @@ import SwiftUI
 struct tutorial: View {
     var body: some View {
         HStack{
-            ZStack {
-                RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 10,height:100).padding(.bottom,50)
-                VStack {
-                    RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 25,height: 10)
-                    RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 50,height: 10)
-                    RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 100,height: 10)
-                    RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 150,height: 10)
-                    
-                }
-//                Spacer()
-            };
-            ZStack {
-                RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 10,height:100).padding(.bottom,50)
-                VStack {
-                    RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 25,height: 10)
-                    RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 50,height: 10)
-                    RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 100,height: 10)
-                    RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 150,height: 10)
-                    
-                }
-//                Spacer()
-            };
-            ZStack {
-                RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 10,height:100).padding(.bottom,50)
-                VStack {
-                    RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 25,height: 10)
-                    RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 50,height: 10)
-                    RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 100,height: 10)
-                    RoundedRectangle(cornerRadius: 10).fill(Color.cyan).frame(width: 150,height: 10)
-                    
-                }
-                
-            };Spacer()
+            Stacks(Stack: 7)
+            Stacks(Stack: 0)
         }
     }
 }
